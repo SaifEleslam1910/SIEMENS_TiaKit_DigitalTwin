062ca1c3259347bc544db1c77596de5892115ce5
SaifEleslam1910 <saif.emad2202@gmail.com>
2025-05-21 14:34:35 +0300

    Update sense_2.cs

--- Scripts/sense_2.cs
+++ Scripts/sense_2.cs
@@ -1,16 +1,20 @@
+using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class sense_2 : MonoBehaviour
 {
+    public TCPClient.OPCUAState getvalue;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   public static int flag_2 = 0;
+    public static int flag_2 =0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("correct"))
+        if (other.CompareTag("correct"))
         {
-            flag_2=1;
+            flag_2 = 1;
             // Add your logic here for when the player enters the trigger zone
         }
     }
+
 }
